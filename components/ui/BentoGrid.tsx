7e9a1d509f6d9d19a9b3bb005bda20b2311b6d49
mhamsha <hamza.shahzad.8692@gmail.com>
2024-11-14 17:36:11 +0500

    Update technology lists in BentoGrid component for accuracy

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -51,8 +51,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["Javascript", "Typescript", "TailwindCSS"];
+  const rightLists = ["ReactJS", "NextJS", "GSAP"];
 
   const [copied, setCopied] = useState(false);
 
